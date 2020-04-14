19d5602605fc4d2624b92d95217639dd7aef04c7
Brian Campbell <brian.campbell.mba18@said.oxford.edu>
2020-04-14 18:16:46 -0500

    p2: almost done, output needs to be formatted

--- problem_2.py
+++ problem_2.py
@@ -1,14 +1,6 @@
 import os
 
-the_list = os.listdir()
-
-print(f'''
-The currenw working directory is...
-{os.getcwd()}\n\n''')
-
-print(os.listdir("testdir\subdir1"))
-
-def find_files(suffix, path):
+def find_files(path, the_end):
     """
     Find all files beneath path with file name suffix.
 
@@ -24,5 +16,29 @@ def find_files(suffix, path):
     Returns:
        a list of paths
     """
-    return None
+    list_of_paths = []
 
+    assert os.listdir(path), "Unable to find the path. Please try resetting the CWD!"
+
+    # This creates the recursive function within the function.
+    def the_recursive(some_item, var_path):
+        new_path = var_path + "/" + some_item
+        if some_item.endswith(the_end):
+            list_of_paths.append(new_path)
+            print("FOUNDS SOMETHING!!!!")
+            print("Appended {}".format(some_item))
+            print("______________________________\n")
+
+        if os.path.isdir(new_path):
+            print("Exploring new path {}...".format(new_path))
+            print("______________________________\n")
+            for i in os.listdir(new_path):
+                the_recursive(i, new_path)
+    
+    for item in os.listdir(path):
+        the_recursive(item, path)
+
+    return list_of_paths
+
+answer = find_files('testdir', ".c")
+print(answer)
