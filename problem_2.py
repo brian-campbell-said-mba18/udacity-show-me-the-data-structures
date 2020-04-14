f7ff585f0b399478af9318c93da107badda997bc
Brian Campbell <brian.campbell.mba18@said.oxford.edu>
2020-04-14 15:05:13 -0500

    p2: added supplemental files

---
+++ problem_2.py
@@ -0,0 +1,28 @@
+import os
+
+the_list = os.listdir()
+
+print(f'''
+The currenw working directory is...
+{os.getcwd()}\n\n''')
+
+print(os.listdir("testdir\subdir1"))
+
+def find_files(suffix, path):
+    """
+    Find all files beneath path with file name suffix.
+
+    Note that a path may contain further subdirectories
+    and those subdirectories may also contain further subdirectories.
+
+    There are no limit to the depth of the subdirectories can be.
+
+    Args:
+      suffix(str): suffix if the file name to be found
+      path(str): path of the file system
+
+    Returns:
+       a list of paths
+    """
+    return None
+
