663b04cf1f23090615affe3304973ba15f6cf5dd
Brian Campbell <brian.campbell.mba18@said.oxford.edu>
2020-04-16 15:24:14 -0500

    p1: ready for first submission

--- problem_1.py
+++ problem_1.py
@@ -169,16 +169,39 @@ class LRU_HashMap:
 
         This code comes from Reference 1 in References.
         '''
+
+        bucket_index = self.get_hash_code(key)
+        head = self.bucket_array[bucket_index]
+        while head is not None:
+            if head.key == key:
+                head.num_of_edits += 1
+                entry_node = LinkedListNode(key, head.value, head.num_of_edits)
+                self.entry_record.enq(entry_node)
+                return head.value
+            head = head.next
+        return -1
+        
+    def make_get(self, key):
+        '''
+        WARNING: Do not manually activate this function! It is only for the make_room() funciton.
+
+        This returns the value of a search node if it exists. Otherwise this function returns none.
+
+        This code comes from Reference 1 in References.
+        '''
+
         bucket_index = self.get_hash_code(key)
         head = self.bucket_array[bucket_index]
         while head is not None:
             if head.key == key:
                 return head.value
             head = head.next
-        return None
+        return -1
     
-    def get_edits(self, key):
+    def make_get_edits(self, key):
         """
+        WARNING: Do not manually activate this function! It is only for the make_room() funciton.
+
         This returns the number of edits of an existing node within the LRU Hashmap structure.
 
         This code comes from Reference 1 in References.
@@ -230,32 +253,35 @@ class LRU_HashMap:
         querries the entry record queue to find the oldest node within the LRU data structure. 
         Once the value of the node matches a node within the LRU hashmap structure, the LRU
         node is removed from the LRU structure.
+
+        Commented Out Print Statements: The print statements were used for debugging purposes.
+        Feel free to uncomment them to debug.
         '''
         while True:
             if self.entry_record.size() == 0:
-                print(f"""
-                Entry record empty, breaking loop now!
-                __________________________________________""")
+                #print(f"""
+                #Entry record empty, breaking loop now!
+                #__________________________________________""")
                 break
             temp = self.entry_record.deq()
-            print(f"""
-            Popped {temp},
-            from entry record. Searching formatching Node 
-            to delete in the LRU Cache Structure...
-            ...
-            """)
-            if self.get(temp.key) == temp.value:
-                if self.get_edits(temp.key) == temp.num_of_edits:
+            #print(f"""
+            #Popped {temp},
+            #from entry record. Searching formatching Node 
+            #to delete in the LRU Cache Structure...
+            #...
+            #""")
+            if self.make_get(temp.key) == temp.value:
+                if self.make_get_edits(temp.key) == temp.num_of_edits:
                     self.delete(temp.key)
-                    print(f"""
-                    Entry record equals LRU record. LRU Node deleted.
-                    Succeess! BREAKING the loop! 
-                    __________________________________________""")
+                    #print(f"""
+                    #Entry record equals LRU record. LRU Node deleted.
+                    #Succeess! BREAKING the loop! 
+                    #__________________________________________""")
                     break
-            print(f"""
-            The entry record did not match the LRU
-            Cache Node. RESTARTING the loop!
-             __________________________________________\n\n""")
+            #print(f"""
+            #The entry record did not match the LRU
+            #Cache Node. RESTARTING the loop!
+            # __________________________________________\n\n""")
                 
     def delete(self, key):
         '''
@@ -279,6 +305,9 @@ class LRU_HashMap:
                 previous = head
                 head = head.next
 
+# Test Case 1: This case edits a lot of values making it more difficult for the LRU_Hashmap Class
+# to identify the Least Recently Used Cache.
+print("Starting Test Case 1.\n")
 test1 = LRU_HashMap(5)
 test1.set("one",1)
 test1.set("two", 2)
@@ -291,7 +320,53 @@ test1.set("four", 4)
 test1.set("five", 5)
 test1.set('six', 7)
 
-print(test1.get("two"))
+print(test1.get("two")) # This should print -1.
+print(test1.get("one")) # This should print 2.
+print(test1.get("six")) # This should print 7.
+print("Moving onto Test Case 2.")
+print("________________________________________\n")
+
+# Test Case 2: This is simply the Udacity Test Case from the boilerplate code for project 1, this project.
+our_cache = LRU_HashMap(5)
+
+our_cache.set(1, 1)
+our_cache.set(2, 2)
+our_cache.set(3, 3)
+our_cache.set(4, 4)
+
+print(our_cache.get(1))       # returns 1
+print(our_cache.get(2))       # returns 2
+print(our_cache.get(9))      # returns -1 because 9 is not present in the cache
+
+our_cache.set(5, 5) 
+our_cache.set(6, 6)
+
+print(our_cache.get(3))      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
+
+print("Moving onto Test Case 3.")
+print("________________________________________\n")
+
+# Test Case 3 Setup: This is simply the Udacity Test Case from the boilerplate code for project 1, this project.
+# However, I modified the first two nodes and gave them None as values.
+test3 = LRU_HashMap()
+
+test3.set(1, None)
+test3.set(2, None)
+test3.set(3, 3)
+test3.set(4, 4)
+
+
+print(test3.get(1))       # returns None
+print(test3.get(2))       # returns None
+print(test3.get(9))      # returns -1 because 9 is not present in the cache
+
+test3.set(5, 5) 
+test3.set(6, 6)
+
+print(test3.get(3))      # returns -1 because the cache reached it's capacity and 3 was the least recently used entry
+
+print("End of Test Cases!")
+print("________________________________________")
 
 # References
 # 1. Udacity: Data Structures & Algorithms Nanodgree; 2. Data Structures; Lesson 5: Maps and Hashing; 8. Hash Maps Notebook
